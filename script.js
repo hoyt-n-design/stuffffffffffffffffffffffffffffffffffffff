b367d3053232c3dcf69206de13e86b7b818db0db
hoyt-n-design <hoyt-n@learningacademy.education>
2026-05-06 09:35:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-// 1. Create the button element
-const dynamicBtn = document.createElement('button');
 
-// 2. Add text and styling
-dynamicBtn.innerText = 'Dynamic Button';
-dynamicBtn.style.backgroundColor = 'lightred';
-
-// 3. Add functionality
-dynamicBtn.addEventListener('click', () => {
-    console.log('Dynamic button clicked!');
-});
-
-// 4. Append it to the page (e.g., to the body)
-document.body.appendChild(dynamicBtn);
